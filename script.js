74bc352437410a38e271eea7324e09b25420b571
verne935 <julioverne935@gmail.com>
2024-11-14 12:08:56 -0600

    Update script.js

--- script.js
+++ script.js
@@ -57,12 +57,12 @@ updateModeText();
 // Detectar pérdida de conexión a Internet y mostrar error 404
 window.addEventListener('offline', function() {
     // Redirigir a la página 404 cuando se pierde la conexión
-    window.location.href = './404.html';
+    window.location.href = '404.html';
 });
 // Detectar reconexión a Internet y redirigir a la interfaz de pedidos sin recargar
 window.addEventListener('online', function() {
     // Redirigir a la interfaz de pedidos al reconectarse
-    window.location.href = './index.html'; // Cambia esta ruta a la URL que deseas en caso de reconexión
+    window.location.href = 'index.html'; // Cambia esta ruta a la URL que deseas en caso de reconexión
 });
 
 if ('serviceWorker' in navigator) {
